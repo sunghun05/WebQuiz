155c132a00a4961998dc8fa7681e7cf9fd23a1a5
sunghun05 <sunghun051107@gmail.com>
2024-05-30 17:34:26 +0900

    ha

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-const title = document.querySelector('.head h1');
-
+const title = document.querySelector('.head');
+let f_s;
 
 let wheel = [0, ];
 let len ;
 window.addEventListener('wheel', (event) => {
+    f_s = title.style.fontSize;
     let status = event.deltaY;
     len = wheel.length;
     wheel.push(status);
-    // console.log(wheel);
-    // console.log(wheel[len]-wheel[len-1]);
-    if(wheel[len]-wheel[len-1] > 0){
-        console.log("up");
-
-    } else if (wheel[len]-wheel[len-1] < 0){
-        title.style.fontSize = 5+'px';
+    console.log(wheel);
+    
+    if(wheel[len] < 0){//down
+        
+        title.style.fontSize = "150px";
+        console.log("down");
+        
+    } else if (wheel[len] > 0){//up
+        
+        title.style.fontSize = "large";
     }
 })
