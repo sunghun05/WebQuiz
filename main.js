56b6464f7db4dcd3b3f61d048299d1e8aacdface
sunghun05 <sunghun051107@gmail.com>
2024-06-05 01:06:28 +0900

    safari_bug_fixes-bug_fixes;

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const login = document.querySelector('#login');
 let f_s;
 let wheel = [0, ];
 let len ;
-let sum = 0;
+// let sum = 0;
 
 window.addEventListener('wheel', (event) => {
     // console.log(bar.getBoundingClientRect().top);
@@ -13,16 +13,16 @@ window.addEventListener('wheel', (event) => {
     let status = event.deltaY;
     len = wheel.length;
     wheel.push(status);
-
-
-    if(wheel[len] < 0 && bar.getBoundingClientRect().top >= 60){//down
-
+    // title.style.margin = '0px';
+    if(wheel[len] < 0 && bar.getBoundingClientRect().top >= 100){//down
+        // title.style.margin = '0px';
         title.style.visibility = 'visible';
-        title.style.fontSize = "150px";
+        title.style.fontSize = "200px";
 
     } else if (wheel[len] > 0){//up
+        /*title.style.margin = '0px';*/
         main.display = '-webkit-inline-box';
-        title.style.fontSize = "xx-large";
+        title.style.fontSize = "60px";
     }
 })
 
--- topic.js
+++ topic.js
@@ -1,45 +1,64 @@
 const topicBu = document.querySelectorAll('.button');
-const backGround = document.getElementById("mar");
+// const backGround = document.getElementById("mar");
+const backGround = document.querySelector('#tpcimg');
 const topics = new Array('math', 'nature', 'universe', 'korean', 'english', 'dutch');
 let topic;
 topicBu[0].addEventListener('click', (e) => {
     topic = topics[0];
-    console.log(topic);
+    //console.log(topic);
+    // backgroundChanger(0);
 })
 topicBu[1].addEventListener('click', (e) => {
     topic = topics[1];
-    console.log(topic);
+    //console.log(topic);
+    // backgroundChanger(1);
 })
 topicBu[2].addEventListener('click', (e) => {
     topic = topics[2];
-    console.log(topic);
+    //console.log(topic);
+    // backgroundChanger(2);
 })
 topicBu[3].addEventListener('click', (e) => {
     topic = topics[3];
-    console.log(topic);
+    //console.log(topic);
+    // backgroundChanger(3);
 })
 topicBu[4].addEventListener('click', (e) => {
     topic = topics[4];
-    console.log(topic);
+    //console.log(topic);
+    // backgroundChanger(4);
 })
 topicBu[5].addEventListener('click', (e) => {
     topic = topics[5];
-    console.log(topic);
+    //console.log(topic);
+    // backgroundChanger(5);
 })
-function backgroundChanger(){
-    switch(topic){
+function backgroundChanger(a){
+    switch(a){
         case 0 : //math
-            backGround.style.backgroundImage = "url()"
+            backGround.style.backgroundImage = "url('sources/math.jpg')";
+            backGround.style.backgroundSize = 'cover';
+            break;
         case 1 : //nature
-            backGround.style.backgroundImage = "url()"
+            backGround.style.backgroundImage = "url('sources/nature.jpg')";
+            backGround.style.backgroundSize = 'cover';
+            break;
         case 2 : //universe
-            backGround.style.backgroundImage = "url()"
+            backGround.style.backgroundImage = "url('sources/universe.jpeg')";
+            backGround.style.backgroundSize = 'cover';
+            break;
         case 3 : //korean
-            backGround.style.backgroundImage = "url()"
+            backGround.style.backgroundImage = "url('sources/korean.jpg')";
+            backGround.style.backgroundSize = 'cover';
+            break;
         case 4 : //english
-            backGround.style.backgroundImage = "url()"
+            backGround.style.backgroundImage = "url('sources/english.jpg')";
+            backGround.style.backgroundSize = 'cover';
+            break;
         case 5 : //dutch
-            backGround.style.backgroundImage = "url()"
+            backGround.style.backgroundImage = "url()";
+            backGround.style.backgroundSize = 'cover';
+            break;
 
     }
 }
