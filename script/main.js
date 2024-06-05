08a862eb499c19a91becf45b53ce488fa57b918e
sunghun05 <sunghun051107@gmail.com>
2024-06-05 20:00:46 +0900

    compatibility_fix;

--- script/main.js
+++ script/main.js
@@ -3,27 +3,25 @@ const title = document.querySelector('.head');
 const bar = document.querySelector('#mar');
 const login = document.querySelector('#login');
 let f_s;
-let wheel = [0, ];
-let len ;
-// let sum = 0;
+// let wheel = [0, ];
 
-window.addEventListener('wheel', (event) => {
-    // console.log(bar.getBoundingClientRect().top);
+window.addEventListener('scroll', (event) => {
+    console.log(bar.getBoundingClientRect().top);
     f_s = title.style.fontSize;
-    let status = event.deltaY;
-    len = wheel.length;
-    wheel.push(status);
+    // let status = event.deltaY;
+    // len = wheel.length;
+    // wheel.push(status);
     // title.style.margin = '0px';
-    if(wheel[len] < 0 && bar.getBoundingClientRect().top >= 140){//down
+    if(bar.getBoundingClientRect().top >= 140){//down
         // title.style.margin = '0px';
-        title.style.visibility = 'visible';
+        // title.style.visibility = 'visible';
         title.style.fontSize = "200px";
 
-    } else if (wheel[len] > 0){//up
+    }
+    if (bar.getBoundingClientRect().top <= 250){//up
         /*title.style.margin = '0px';*/
-        main.display = '-webkit-inline-box';
+        // main.display = '-webkit-inline-box';
         title.style.fontSize = "60px";
     }
 })
-
 
